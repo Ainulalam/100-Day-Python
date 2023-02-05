d60b9d4c51627aeb1a64d420872431b6687ef689
AINUL ALAM <104184992+Ainulalam@users.noreply.github.com>
2023-02-05 15:24:41 +0000

    add 2 sting number in digit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print('Hello, world!')
 
-name=input("What is your name ? ")
-print(type(name))
+num =input("Enter two digit Number:  ")
+
+first_digit=num[0]
+second_digit=num[1]
+print(int(first_digit) + int(second_digit))
